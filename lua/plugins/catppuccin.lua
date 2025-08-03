6aebb0f1d8d91f7d006510bbe29a7012f9be489a
DJ O'Leary <dijitol@proton.me>
2025-08-03 15:55:44 +0200

    refactor(catppuccin): update config

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -5,11 +5,19 @@ return {
     name = "catppuccin",
     priority = 1000,
     opts = {
-      flavour = "mocha",
+      flavour = "auto",
+      background = {
+        light = "latte",
+        dark = "mocha",
+      },
+      transparent_background = true,
       integrations = {
         blink_cmp = true,
+        dap = true,
+        dap_ui = true,
         harpoon = true,
         lsp_trouble = true,
+        nvim_surround = true,
       },
     },
   },
